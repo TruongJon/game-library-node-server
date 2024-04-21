a29dbcbe7d5988730205ffc2951b4d94976d5d01
philipngpack <ngpack.p@northeastern.edu>
2024-04-21 19:48:34 -0400

    change json formatting of riot routes

--- Clients/riot.js
+++ Clients/riot.js
@@ -8,8 +8,12 @@ export default function RiotRoutes(app) {
         const summonerName = req.params.summonerName;
 
         try {
-            const summoner = await client.summoners.fetchBySummonerName(summonerName);
-            res.json(summoner.data);
+            const summoner = (await client.summoners.fetchBySummonerName(summonerName));
+            res.json({
+                summonerName: summoner.name,
+                summonerLevel: summoner.level,
+                summonerIcon: summoner.profileIcon
+            });
         } catch (err) {
             console.error(err);
             res.status(400).send('An error occurred while fetching data from the Riot API.');
@@ -23,7 +27,11 @@ export default function RiotRoutes(app) {
             const summoner = await client.summoners.fetchBySummonerName(summonerName);
             const leagueEntry = await summoner.fetchLeagueEntries();
             const soloQ = leagueEntry.get('RANKED_SOLO_5x5');
-            res.json(soloQ.data);
+            res.json({
+                tier: soloQ.tier,
+                division: soloQ.division,
+                lp: soloQ.lp
+            });
         } catch (err) {
             console.error(err);
             res.status(400).send('An error occurred while fetching data from the Riot API.');
@@ -37,7 +45,11 @@ export default function RiotRoutes(app) {
             const summoner = await client.summoners.fetchBySummonerName(summonerName);
             const leagueEntry = await summoner.fetchLeagueEntries();
             const flexQ = leagueEntry.get('RANKED_FLEX_SR');
-            res.json(flexQ.data);
+            res.json({
+                tier: flexQ.tier,
+                division: flexQ.division,
+                lp: flexQ.lp
+            });
         } catch (err) {
             console.error(err);
             res.status(400).send('An error occurred while fetching data from the Riot API.');
@@ -46,18 +58,44 @@ export default function RiotRoutes(app) {
 
     const getHighestMastery = async (req, res) => {
         const summonerName = req.params.summonerName;
-
+    
         try {
             const summoner = await client.summoners.fetchBySummonerName(summonerName);
             const championMastery = summoner.championMastery;
             const highest = await championMastery.fetchTop(5);
-            res.json(highest.data);
+            res.json({
+                first: {
+                    name: highest[0].champion.name,
+                    level: highest[0].level,
+                    points: highest[0].points
+                },
+                second: {
+                    name: highest[1].champion.name,
+                    level: highest[1].level,
+                    points: highest[1].points
+                },
+                third: {
+                    name: highest[2].champion.name,
+                    level: highest[2].level,
+                    points: highest[2].points
+                },
+                fourth: {
+                    name: highest[3].champion.name,
+                    level: highest[3].level,
+                    points: highest[3].points
+                },
+                fifth: {
+                    name: highest[4].champion.name,
+                    level: highest[4].level,
+                    points: highest[4].points
+                }
+            });
         } catch (err) {
             console.error(err);
             res.status(400).send('An error occurred while fetching data from the Riot API.');
         }
     };
-
+    
     app.get('/api/summoner/:summonerName', getSummoner);
     app.get('/api/soloq/:summonerName', getSoloQ);
     app.get('/api/flexq/:summonerName', getFlexQ);

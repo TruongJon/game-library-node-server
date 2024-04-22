709c98772986c9ad7786d65fe2223bad1af057c4
Lucy Ying <lucyying@Lucys-MacBook-Pro.local>
2024-04-22 01:20:46 -0400

    init commit

--- Users/dao.js
+++ Users/dao.js
@@ -15,3 +15,8 @@ export const findUserByUsername = (username) =>  model.findOne({ username: usern
 export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
 export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const searchUsername = (usernamePartial, following) =>
+  model.find({ $and :
+    [{username: { $regex: `^${usernamePartial}.*`, $options: "mi" }},
+    {username: { $nin: following }}],
+  });

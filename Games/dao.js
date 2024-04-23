839eb2cecfd432c1343e7655248144cce5ae8ad4
philipngpack <ngpack.p@northeastern.edu>
2024-04-23 01:23:15 -0400

    delete before adding games

--- Games/dao.js
+++ Games/dao.js
@@ -1,12 +1,15 @@
 import model from "./model.js";
-export const createGame = (game) => {
-    model.deleteOne({ gameName: game.gameName});
+
+export const createGame = async (game) => {
+    delete game._id;
     const emptyGame = {
         origin: '',
         gameName: '',
         image_url: '',
     };
+    await model.deleteOne({ gameName: game.gameName });
     return model.create({...emptyGame, ...game});
 }
+
 export const findAllGames = () => model.find();
 export const findGameByGameName = (gameName) =>  model.findOne({ gameName: gameName });
